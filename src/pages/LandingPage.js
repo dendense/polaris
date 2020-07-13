f4e1708c75e837209aefc27d2c67b57ada11ecad
DenDense <denyfarras@gmail.com>
2020-07-13 23:53:07 +0700

    LandingPage Update

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -11,9 +11,25 @@ import img7 from 'assets/images/7.jpg'
 export default class LandingPage extends Component {
     render() {
         return (
-            <>
-                
-            </>
+            <section className="container">
+                <div className="container-grid">
+                    <div className="item column-4 row-2">
+                        <div className="card">Card 1</div>
+                    </div>
+                    <div className="item column-4 row-1">
+                        <div className="card">Card 2</div>
+                    </div>
+                    <div className="item column-4 row-1">
+                        <div className="card">Card 3</div>
+                    </div>
+                    <div className="item column-4 row-1">
+                        <div className="card">Card 4</div>
+                    </div>
+                    <div className="item column-4 row-1">
+                        <div className="card">Card 5</div>
+                    </div>
+                </div>
+            </section>
         );
     }
 }
