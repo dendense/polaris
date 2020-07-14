21a8bef3468f87a607fb040a5af7d9a775fd1b02
DenDense <denyfarras@gmail.com>
2020-07-14 18:43:04 +0700

    LoopingGrid Landing Page

--- src/parts/FrontGrid.js
+++ src/parts/FrontGrid.js
@@ -3,12 +3,12 @@ import Button from "elements/Button";
 
 export default function FrontGrid(props) {
   return (
-    <section className="container">
-      <div className="container-grid no-grid-gutters">
+    <section className="container-flex">
+      <div className="container-grid">
         {
           props.data.map((item, index) => {
             return(
-              <div class={`item no-grid-gutters column-3${index === 0 ? " row-2" : " row-1"}`}>
+              <div class={`item column-2${index === 0 ? " row-2" : " row-1"}`}>
                 <div class="card card-featured">
                   <div class="img-wrapper">
                     <img src={item.imageUrl} alt={item.name} className="img-cover"/>
