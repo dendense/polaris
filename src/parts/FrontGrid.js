102f921be2ff99c12960949a38715f080ea0cacc
DenDense <denyfarras@gmail.com>
2020-07-19 20:05:52 +0700

    LandingPage Update

--- src/parts/FrontGrid.js
+++ src/parts/FrontGrid.js
@@ -4,20 +4,22 @@ import data from 'json/homeData';
 
 export default function FrontGrid(props) {
   return (
-    <div className="grid-container">
-      <section className="cols grid-area">
-        {
-          data.homePict.map((homepict, index) => {
-            return (
-              <>
-                <div className={homepict.grid}>
-                  <img src={homepict.imgUrl} alt={homepict.alt} className="img-cover" />
-                </div>
-              </>
-            );
-          })
-        }
-      </section>
-    </div>
+    <section className="main-front">
+      <div className="container-flex">
+        <div className="grid-area border">
+          {
+            data.homePict.map((homepict, index) => {
+              return (
+                <>
+                  <div className={homepict.grid}>
+                    <img src={homepict.imgUrl} alt={homepict.alt} className="img-cover" />
+                  </div>
+                </>
+              );
+            })
+          }
+        </div>
+      </div>
+    </section>
   );
 }
