e1ae234871d627a656790a9911b3cfc62759c2df
DenDense <denyfarras@gmail.com>
2020-07-20 18:18:47 +0700

    LandingPage Update

--- src/parts/FrontGrid.js
+++ src/parts/FrontGrid.js
@@ -1,25 +1,24 @@
 import React from "react";
 import Button from "elements/Button";
 import data from 'json/homeData';
+import pict1 from 'assets/images/12.jpg';
+import pict2 from 'assets/images/14.jpg';
+import pict3 from 'assets/images/13.jpg';
+import pict4 from 'assets/images/10.jpg';
+import pict5 from 'assets/images/11.jpg';
 
 export default function FrontGrid(props) {
   return (
-    <section className="main-front">
-      <div className="container-flex">
-        <div className="grid-area border">
-          {
-            data.homePict.map((homepict, index) => {
-              return (
-                <>
-                  <div className={homepict.grid}>
-                    <img src={homepict.imgUrl} alt={homepict.alt} className="img-cover" />
-                  </div>
-                </>
-              );
-            })
-          }
-        </div>
-      </div>
-    </section>
+    <div class="grid-area">
+      <div class="grd grid-1"><img src={pict3} alt="polaris" className="img-cover"/></div>
+      <div class="grd grid-2"><img src={pict2} alt="polaris" className="img-cover"/></div>
+      <div class="grd grid-2"><img src={pict1} alt="polaris" className="img-cover"/></div>
+      <div class="grd grid-3"><img src={pict5} alt="polaris" className="img-cover"/></div>
+      <div class="grid-4"><div class="text-hero"><span>POLARIS</span></div></div>
+      <div class="grd grid-2"><img src={pict5} alt="polaris" className="img-cover"/></div>
+      <div class="grd grid-2"><img src={pict5} alt="polaris" className="img-cover"/></div>
+      <div class="grd grid-2"><img src={pict5} alt="polaris" className="img-cover"/></div>
+      <div class="grd grid-5"><img src={pict3} alt="polaris" className="img-cover"/></div>
+    </div>
   );
 }
