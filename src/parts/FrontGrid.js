8e4d0e1dbe7da0289856341317c18df55febf3ad
DenDense <denyfarras@gmail.com>
2020-07-24 21:06:47 +0700

    LandingPage Fixed

--- src/parts/FrontGrid.js
+++ src/parts/FrontGrid.js
@@ -4,16 +4,46 @@ import IconText from './IconText.js';
 
 export default function FrontGrid(props) {
   return (
-    <div className="grid-area">
-      {
-        props.data.map((homepict, index) => {
-          return (
-            <div key={`${index}`} className={"img-wrapper" + homepict.grid}>
-              <img src={homepict.imgUrl} alt={homepict.alt} className="img-cover" />
-            </div>
-          )
-        })
-      }
-    </div>
+    <>
+      <section className="login-container">
+        <div className="log-btn">
+          <Button
+            type="link"
+            className="btn btn-primary text-white"
+            href={`/properties/`}
+          >
+            Log In / Sign Up
+            </Button>
+        </div>
+      </section>
+      <section className="faq-container">
+        <div className="faq-btn">
+          <Button
+            type="link"
+            className="btn btn-warning text-black"
+            href={`/properties/`}
+          >
+            Terms &amp; Conditions
+            </Button>
+        </div>
+      </section>
+      <section className="icon-container">
+        <div className="icon-btn">
+          <IconText/>
+        </div>
+      </section>
+      <div className="grid-area">
+        {
+          props.data.map((homepict, index) => {
+
+            return (
+              <div key={`${index}`} className={"img-wrapper" + homepict.grid}>
+                <img src={homepict.imgUrl} alt={homepict.alt} className="img-cover" />
+              </div>
+            )
+          })
+        }
+      </div>
+    </>
   );
 }
