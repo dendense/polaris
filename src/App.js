2928c216e38ce49a74960f4eda00958f668b7a61
DenDense <denyfarras@gmail.com>
2020-07-29 21:15:33 +0700

    Modal Components Update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route} from 'react-router-dom'
 
 import 'assets/scss/style.scss'
 import LandingPage from 'pages/LandingPage';
-import TimeLine from 'pages/TimeLine';
+import TestBlank from 'pages/TestBlank';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={TimeLine}></Route>
+        <Route path="/" component={TestBlank}></Route>
       </Router>
     </div>
   );
--- src/elements/Modal/index.js
+++ src/elements/Modal/index.js
@@ -1,9 +1,39 @@
-import React from "react";
-export default class Modal extends React.Component {
-  render() {
-    if(!this.props.show){
-        return null;
-    }  
-    return <div>{this.props.children}</div>;
-  }
+import React from 'react'
+import Button from 'elements/Button';
+
+const Modal = ({ handleClose, show, children, headModal }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+  
+  return (
+    <div
+      className={showHideClassName + `fade`}
+      id="staticBackdrop"
+      data-backdrop="static"
+      data-keyboard="false"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">{headModal}</h5>
+          </div>
+        </div>
+      </div>
+      {/* {{children}
+        <Button
+          type="button"
+          className="btn btn-info"
+          onClick={handleClose}
+          href={``}
+        >
+          Close
+        </Button>} */}
+    </div>
+    
+  );
 }
+
+export default Modal
