9724218c1a7a4a9f195cd9ec4bbc77f06e82d925
DenDense <denyfarras@gmail.com>
2020-08-07 22:59:18 +0700

    Adding InputText Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import 'assets/scss/style.scss'
-import LandingPage from 'pages/LandingPage';
-import TestBlank from 'pages/TestBlank';
+import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
+import TestBlank from "pages/TestBlank";
+import VerificationPage from "pages/VerificationPage";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={TestBlank}></Route>
+        <Route path="/" component={VerificationPage}></Route>
       </Router>
     </div>
   );
