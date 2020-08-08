82d6c9b639f634085a6cd4ced29e1f34f85dbe37
DenDense <denyfarras@gmail.com>
2020-08-08 23:04:40 +0700

    Adding Header Function Component

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -3,16 +3,16 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
-import TestBlank from "pages/TestBlank";
 import VerificationPage from "pages/VerificationPage";
-import LoginTabs from "parts/LoginTabs";
+import Header from "parts/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/verification-page" component={VerificationPage} />
+        <Route exact path="/" component={Header} />
+        {/* <Route exact path="/" component={LandingPage} />
+        <Route path="/verification-page" component={VerificationPage} /> */}
       </Router>
     </div>
   );
