563e55b33bf818a706a5f9ad78c99c795d2dda89
IkhsanNurulRizki <ikhsan.n.rizki@gmail.com>
2020-08-17 20:27:18 +0700

    update IkhsanComponent

--- src/pages/ikhsan/index.js
+++ src/pages/ikhsan/index.js
@@ -4,22 +4,19 @@ import "./index.scss";
 export default class IkhsanComponent extends Component {
   render() {
     return (
-      <div className="container-fluid">
+      <div className="container-fluid d-flex timeline-ikhsan" id="fullscreen">
         <div className="kiri">
-          <div>
+          <div className="sidebar">
             <h1>sidebar</h1>
           </div>
-          {/* sidebar */}
         </div>
-        <div className="kanan">
-          <div>
+        <div className="container-fluid" id="fullscreen">
+          <div className="header">
             <h1>header</h1>
           </div>
-          {/* header */}
-          <div>
+          <div className="body">
             <h1>body</h1>
           </div>
-          {/* body */}
         </div>
       </div>
     );
