db0ea2211b2827d73a4269d936ed2c2a0f520779
IkhsanNurulRizki <ikhsan.n.rizki@gmail.com>
2020-08-30 11:49:38 +0700

    update body

--- src/pages/ikhsan/BodyIkhsan.js
+++ src/pages/ikhsan/BodyIkhsan.js
@@ -2,13 +2,15 @@ import React from "react";
 
 export default function BodyIkhsan(props) {
   return (
-    <div className="div-body text-white">
-      <h1>BODY</h1>
-      <div className="konten-bottom">
-        <span>Back to Top </span>
-        <a className="btn btn-primary" href="#top">
-          Click Here
-        </a>
+    <div className="div-body">
+      <div className="container-sm bg-dark">
+        <h1>BODY</h1>
+        <div className="konten-bottom">
+          <span>Back to Top </span>
+          <a className="btn btn-primary" href="#top">
+            Click Here
+          </a>
+        </div>
       </div>
     </div>
   );
