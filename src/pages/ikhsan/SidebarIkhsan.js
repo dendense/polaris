43b11e17de52fae3e7286ab97156e25001d351d2
IkhsanNurulRizki <ikhsan.n.rizki@gmail.com>
2020-08-30 11:23:20 +0700

    create navbar

--- src/pages/ikhsan/SidebarIkhsan.js
+++ src/pages/ikhsan/SidebarIkhsan.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function SidebarIkhsan() {
   return (
-    <div className="div-side">
-      <h1>SIDEBAR</h1>
+    <div className="div-side bg-dark">
+      <img src="1.jpg" class="rounded mx-auto d-block" alt="1.jpg"></img>
     </div>
   );
 }
