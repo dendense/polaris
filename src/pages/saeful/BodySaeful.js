57a376ae5d51148f1437a7dd2911cb8c254b7f15
mrOctopusSA001 <saefulanwaroktariansah@gmail.com>
2020-09-07 22:41:58 +0700

    materi baru

--- src/pages/saeful/BodySaeful.js
+++ src/pages/saeful/BodySaeful.js
@@ -1,9 +1,12 @@
 import React from "react";
+import FotoGrid from "./FotoGrid";
+import image1 from "assets/images/1.jpg";
 
 export default function BodySaeful(props) {
   return (
-    <div className="div-body m-3 text-white">
+    <div className="div-body m-3">
       <h1>BODY</h1>
+      <FotoGrid imageGrid={image1} />
       <div className="konten-bottom">
         <span>Back to Top </span>
         <a className="btn btn-primary" href="#top">
