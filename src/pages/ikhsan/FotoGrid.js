f6aa2f078ee7b214c2ec84516903ecc169aac883
IkhsanNurulRizki <ikhsan.n.rizki@gmail.com>
2020-09-07 22:48:57 +0700

    potogrid updates

---
+++ src/pages/ikhsan/FotoGrid.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from "elements/Button";
+
+const FotoGrid = (props, data) => {
+  return (
+    <div className="container mt-5 ml-5">
+      <div className="row">
+        <div className="card" style={{ width: "18rem", marginRight: "1rem" }}>
+          <img src={props.imageGrid} className="card-img-top" alt="foto" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem", marginRight: "1rem" }}>
+          <img src={props.imageGrid} className="card-img-top" alt="foto" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FotoGrid;
